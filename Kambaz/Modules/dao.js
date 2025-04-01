9424ab56f8730e66dfb14556e041d915318b298e
abbyelbaum <abby.elbaum@gmail.com>
2025-03-31 22:06:09 -0400

    Creating Modules

--- Kambaz/Modules/dao.js
+++ Kambaz/Modules/dao.js
@@ -1,5 +1,13 @@
 import Database from "../Database/index.js";
+import { v4 as uuidv4 } from "uuid";
+
 export function findModulesForCourse(courseId) {
   const { modules } = Database;
   return modules.filter((module) => module.course === courseId);
-}
+};
+
+export function createModule(module) {
+    const newModule = { ...module, _id: uuidv4() };
+    Database.modules = [...Database.modules, newModule];
+    return newModule;
+}  
