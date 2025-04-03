eaf046ee28bf40867b68750dae4466b9917d8dc5
abbyelbaum <abby.elbaum@gmail.com>
2025-04-02 21:41:22 -0400

    Enroll button works

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -2,9 +2,13 @@ import Database from "../Database/index.js";
 import { v4 as uuidv4 } from "uuid";
 
 export function enrollUserInCourse(userId, courseId) {
-    const {enrollments}  = Database;
-    enrollments.push({_id: uuidv4(), user: userId, course: courseId});
+    const { enrollments } = Database;
+    const newEnrollment = { _id: uuidv4(), user: userId, course: courseId };
+    enrollments.push(newEnrollment);
+    console.log("Updated Enrollments:", enrollments);
 }
+
+
 
 export function unenrollUserFromCourse(userId, courseId) {
     Database.enrollments = Database.enrollments.filter(
--- Kambaz/Enrollments/routes.js
+++ Kambaz/Enrollments/routes.js
@@ -2,8 +2,12 @@ import * as dao from "./dao.js";
 
 export default function EnrollmentRoutes(app) {
   app.post("/api/enrollments", (req, res) => {
-    const { userId, courseId } = req.body;
-    dao.enrollUserInCourse(userId, courseId);
+    const { courseId } = req.body;
+    const currentUser = req.session["currentUser"];
+    if (!currentUser) {
+      return res.sendStatus(401);
+    }
+    dao.enrollUserInCourse(currentUser._id, courseId);
     res.sendStatus(201);
   });
 
