6b0090a8ef5fde36f08c8732268dc80194b8e926
abbyelbaum <abby.elbaum@gmail.com>
2025-04-06 16:00:34 -0400

    CRUD Courses

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -3,21 +3,21 @@ import * as modulesDao from "../Modules/dao.js";
 import * as assignmentsDao from "../Assignments/dao.js";
 
 export default function CourseRoutes(app) {
-  app.get("/api/courses", (req, res) => {
-    const courses = dao.findAllCourses();
+  app.get("/api/courses", async (req, res) => {
+    const courses = await dao.findAllCourses();
     res.send(courses);
   });
 
-  app.delete("/api/courses/:courseId", (req, res) => {
+  app.delete("/api/courses/:courseId", async (req, res) => {
     const {courseId} = req.params;
-    const status = dao.deleteCourse(courseId);
+    const status = await dao.deleteCourse(courseId);
     res.send(status);
   });
 
-  app.put("/api/courses/:courseId", (req, res) => {
+  app.put("/api/courses/:courseId", async (req, res) => {
     const { courseId } = req.params;
     const courseUpdates = req.body;
-    const status = dao.updateCourse(courseId, courseUpdates);
+    const status = await dao.updateCourse(courseId, courseUpdates);
     res.send(status);
   });
 
@@ -51,5 +51,9 @@ export default function CourseRoutes(app) {
     };
     const newAssignment = assignmentsDao.createAssignment(assignmnet);
     res.send(newAssignment);
+  });
+  app.post("/api/courses", async (req, res) => {
+    const course = await dao.createCourse(req.body);
+    res.json(course);
   });
 }
